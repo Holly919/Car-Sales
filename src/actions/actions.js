1c8f0fa4d262eccbe2af8fa0af146bc900c55ed4
Holly919 <56270260+Holly919@users.noreply.github.com>
2020-07-27 13:59:46 -0400

    still debugging

---
+++ src/actions/actions.js
@@ -0,0 +1,26 @@
+export const REMOVE_FEATURE = "REMOVE_FEATURE";
+export const ADD_FEATURE = "ADD_FEATURE"
+export const UPDATE_PRICE = "UPDATE_PRICE";
+
+export const addFeature = (feature) => {
+
+  return {
+    type: ADD_FEATURE,
+    payload: feature,
+  };
+};
+
+export const removeFeature = (id) => {
+    return {
+    type: REMOVE_FEATURE,
+    payload: id,
+  };
+};
+
+export const updatePrice = (version, price) => {
+  return {
+    type: UPDATE_PRICE,
+    payload: price,
+    version: version,
+  };
+};
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const AdditionalFeature = props => {
+const AddedFeature = (props) => {
+  const removeCurFeature = () => {
+    let id = props.feature.id;
+    props.removeFeature(id);
+    props.updatePrice("subtract", props.feature.price);
+  };
+
   return (
     <li>
-      {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
-      {props.feature.name} (+{props.feature.price})
+      {/* Add an onClick to run a function to remove a feature */}
+      <button className="button" onClick={removeCurFeature}>
+        X
+      </button>
+      {props.feature.name}
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default AddedFeature;
